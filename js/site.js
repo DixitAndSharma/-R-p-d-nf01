f9e4a2a294702dfbb5fa653838ca81c9fc0540ed
bhawnadixit <dixitbhawna.15@gmail.com>
2014-09-25 17:45:54 +0530

    Image

---
+++ js/site.js
@@ -0,0 +1,13 @@
+﻿(function ()
+{
+    var curImgId = 0;
+    var numberOfImages = 3; // Change this to the number of background images
+    window.setInterval(function ()
+    {
+        $('body').css('background-image', 'url(/image/background' + curImgId + '.jpg)');
+        curImgId = (curImgId+1) % numberOfImages;
+    }, 5 * 1000);
+    
+})
+
+();
